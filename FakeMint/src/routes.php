468c637f4a8288c87cd23f78857a11dce37e05db
Edward Odre <eodre@smu.edu>
2018-11-07 14:57:29 -0600

    cors

--- FakeMint/src/routes.php
+++ FakeMint/src/routes.php
@@ -3,6 +3,21 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+  return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+  $response = $next($req, $res);
+  return $response
+          ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+          ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+          ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+
 // Routes
 
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
@@ -157,4 +172,10 @@ $app->group('/api', function () use ($app) {
       return $this->response->withJson($input); 
     });
 });
+
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+  $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+  return $handler($req, $res);
+});
+
 
