9097e8ae35f7d0dabb93769a3d45aaeef5c09418
Edward Odre <eodre@smu.edu>
2018-11-27 11:51:49 -0600

    test 2.3

--- FakeMint/src/routes.php
+++ FakeMint/src/routes.php
@@ -362,8 +362,10 @@ $app->group('/api', function () use ($app) {
       $secST = var_dump($array[1]['exType']);
 
       $quer = $this->db->prepare(
-        "SELECT * FROM suggs WHERE suggType = $firstST OR $secST"
+        "SELECT * FROM suggs WHERE suggType = firstST OR $secST"
       );
+      $quer = bindParam("firstST", $firstST);
+      $quer = bindParam("secST", $secST);
       $quer->execute();
       $suggs = $quer->fetchAll();
 
