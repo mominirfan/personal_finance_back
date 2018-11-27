c788d35fbfbdef5a559e005f285cbaa02f234031
Edward Odre <eodre@smu.edu>
2018-11-27 12:13:12 -0600

    test 2.4

--- FakeMint/src/routes.php
+++ FakeMint/src/routes.php
@@ -359,7 +359,7 @@ $app->group('/api', function () use ($app) {
       $sth->execute();
       $types = $sth->fetchAll();
       $jsonTypes = json_encode($types);
-      $array = json_decode($json_Types,true);
+      $array = json_decode($jsonTypes,true);
       $firstST = var_dump($array[0]['exType']);
       $secST = var_dump($array[1]['exType']);
       
