d30683b85789ff0fd82d14f60ada20d72bd44329
Edward Odre <eodre@smu.edu>
2018-11-28 13:42:15 -0600

    password change verification

--- FakeMint/src/routes.php
+++ FakeMint/src/routes.php
@@ -77,7 +77,7 @@ $app->group('/api', function () use ($app) {
       VALUES (:userName, :budgetType, now(), 0)";
       
       $budget_sth = $this->db->prepare($budget_sql);
-      $types = array("Savings","Ent.","Util.","Food","Car","House","Misc.");
+      $types = array("Savings","Ent.","Util.","Food","Car","House","Misc.","Fun");
       $budget_sth->bindParam("userName", $input['userName']);
       foreach($types as $type){
         $budget_sth->bindParam("budgetType", $type); 
@@ -88,18 +88,12 @@ $app->group('/api', function () use ($app) {
       VALUES (:userName, :exType, now(), 0)";
       
       $ex_sth = $this->db->prepare($ex_sql);
-      $extypes = array("Ent.","Util.","Food","Car","House","Misc.");
+      $extypes = array("Ent.","Util.","Food","Car","House","Misc.","Fun");
       $ex_sth->bindParam("userName", $input['userName']);
       foreach($extypes as $extype){
         $ex_sth->bindParam("exType", $extype); 
         $ex_sth->execute();
       }
-
-
-      
-      
-      
-      
       return $this->response->withJson($input); 
     });
 
