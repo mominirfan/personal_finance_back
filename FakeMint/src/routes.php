c9ef2aa205327a15eb52643e45e234bad0f78e38
Julie Tapia <jtapia@smu.edu>
2018-11-28 23:52:54 -0600

    alright i found the error

--- FakeMint/src/routes.php
+++ FakeMint/src/routes.php
@@ -227,7 +227,7 @@ $app->group('/api', function () use ($app) {
       $day = $input['paymentDay'];
       $isDate = checkdate($month, $day, $year);
       if($isDate == false){
-        $date = mktime(0, 0, 0, $month, 0, $year);
+        $date = mktime(0, 0, 0, $month+1, 0, $year);
         $day = date('d', $date);
         $input['paymentDay'] = $day;
       }
