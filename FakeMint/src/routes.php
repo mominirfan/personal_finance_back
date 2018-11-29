64bf2b91819ac7eae7c76971c437139d576f10f7
Julie Tapia <jtapia@smu.edu>
2018-11-28 21:11:41 -0600

    send reg error

--- FakeMint/src/routes.php
+++ FakeMint/src/routes.php
@@ -60,41 +60,52 @@ $app->group('/api', function () use ($app) {
     $app->post('/registration', function ($request, $response) {
       #creates new user
       $input = $request->getParsedBody();
-      $sql = "INSERT INTO users (userName, pWord, lastName, firstName, email, income, bal) 
-      VALUES (:userName, :pWord, :lastName, :firstName, :email, :income, :bal)";
-      $sth = $this->db->prepare($sql);
-      $sth->bindParam("userName", $input['userName']);
-      $sth->bindParam("pWord", $input['pWord']);
-      $sth->bindParam("lastName", $input['lastName']);
-      $sth->bindParam("firstName", $input['firstName']);
-      $sth->bindParam("email", $input['email']);
-      $sth->bindParam("income", $input['income']);
-      $sth->bindParam("bal", $input['bal']);
-      $sth->execute();
-      #creates budget items init to 0
-      
-      $budget_sql = "INSERT INTO budgets (userName, budgetType, active_date, amt) 
-      VALUES (:userName, :budgetType, now(), 0)";
-      
-      $budget_sth = $this->db->prepare($budget_sql);
-      $types = array("Savings","Ent.","Util.","Food","Car","House","Misc.");
-      $budget_sth->bindParam("userName", $input['userName']);
-      foreach($types as $type){
-        $budget_sth->bindParam("budgetType", $type); 
-        $budget_sth->execute();
+      $sql = $this->db->prepare(
+        "SELECT userName FROM users WHERE userName=:userName"
+      );
+      $sql->bindParam("userName", $input['userName']);
+      $sql->execute();
+      $check = $sql->fetchObject();
+      if($check){
+        $qr = "INSERT INTO users (userName, pWord, lastName, firstName, email, income, bal) 
+        VALUES (:userName, :pWord, :lastName, :firstName, :email, :income, :bal)";
+        $sth = $this->db->prepare($qr);
+        $sth->bindParam("userName", $input['userName']);
+        $sth->bindParam("pWord", $input['pWord']);
+        $sth->bindParam("lastName", $input['lastName']);
+        $sth->bindParam("firstName", $input['firstName']);
+        $sth->bindParam("email", $input['email']);
+        $sth->bindParam("income", $input['income']);
+        $sth->bindParam("bal", $input['bal']);
+        $sth->execute();
+        #creates budget items init to 0
+        
+        $budget_sql = "INSERT INTO budgets (userName, budgetType, active_date, amt) 
+        VALUES (:userName, :budgetType, now(), 0)";
+        
+        $budget_sth = $this->db->prepare($budget_sql);
+        $types = array("Savings","Ent.","Util.","Food","Car","House","Misc.");
+        $budget_sth->bindParam("userName", $input['userName']);
+        foreach($types as $type){
+          $budget_sth->bindParam("budgetType", $type); 
+          $budget_sth->execute();
+        }
+        #creates expenses init to 0
+        $ex_sql = "INSERT INTO expenses (userName, exType, date, amt) 
+        VALUES (:userName, :exType, now(), 0)";
+        
+        $ex_sth = $this->db->prepare($ex_sql);
+        $extypes = array("Ent.","Util.","Food","Car","House","Misc.","Savings");
+        $ex_sth->bindParam("userName", $input['userName']);
+        foreach($extypes as $extype){
+          $ex_sth->bindParam("exType", $extype); 
+          $ex_sth->execute();
+        }
+        return $this->response->withJson($input);
+      } else {
+        return $this->response->withJson(['error' => true, 'message' => 'Username already in use']);
       }
-      #creates expenses init to 0
-      $ex_sql = "INSERT INTO expenses (userName, exType, date, amt) 
-      VALUES (:userName, :exType, now(), 0)";
-      
-      $ex_sth = $this->db->prepare($ex_sql);
-      $extypes = array("Ent.","Util.","Food","Car","House","Misc.","Savings");
-      $ex_sth->bindParam("userName", $input['userName']);
-      foreach($extypes as $extype){
-        $ex_sth->bindParam("exType", $extype); 
-        $ex_sth->execute();
-      }
-      return $this->response->withJson($input); 
+       
     });
 
     $app->get('/login/[{userName}]', function ($request, $response, $args) {
@@ -230,15 +241,15 @@ $app->group('/api', function () use ($app) {
 
     $app->get('/test', function ($request, $response) {
       $input = $request->getParsedBody();
-      $user = $input['userName'];
       $sql = $this->db->prepare(
         "SELECT userName FROM users WHERE userName=:userName"
       );
       $sql->bindParam("userName", $input['userName']);
       $sql->execute();
       $check = $sql->fetchObject();
-      #if($check)
-      return $this->response->withJson($check); 
+      if($check){
+        return $this->response->withJson($input);
+      } 
     });
 
     $app->put('/edit-loan', function ($request, $response, $args) {
